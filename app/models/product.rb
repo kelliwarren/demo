3683ca2f96cbef7e4a6b8f0308d4a7d3670dc246
kelliwarren <kmdesigngroup@gmail.com>
2014-03-19 11:08:28 -0700

    fixed bad code

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,4 @@
 class Product < ActiveRecord::Base
-  validatates :title, :description, :image_url, presence: true
-  validatates :price, numericality: {greater_than_or_equal_to: 0.01}
+  validates :title, :description, :image_url, presence: true
+  validates :price, numericality: {greater_than_or_equal_to: 0.01}
 end
