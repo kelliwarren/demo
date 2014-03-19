a7c682dfb70b42c29b259fa752bd4cf217638268
DPL-1 <DPL-1@DPL-1s-iMac.local>
2014-03-19 09:12:42 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Demo::Application.config.secret_key_base = '81298ab5192a9a6e0b5c423ebb2b27a5c4d14261ac59772c8a0ce68164c8ccd7f52df4fc2a0d2a2265e8c9472c9b2856b77794904d5d5e964ee64fee99c4d791'
